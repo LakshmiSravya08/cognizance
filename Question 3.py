85b3d3674f03e99d7bfd1067ed54ec5231c38773
LakshmiSravya08 <99632838+LakshmiSravya08@users.noreply.github.com>
2022-03-09 18:22:23 +0530

    Add files via upload

---
+++ Question 3.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+
+print(0 * np.nan)
+
+
+# In[2]:
+
+
+print(np.nan != np.nan)
+
+
+# In[3]:
+
+
+print(np.inf > np.nan) 
+
+
+# In[4]:
+
+
+print(np.nan - np.nan)
+
+
+# In[5]:
+
+
+print(0.3 == 3 * 0.1) 
+
+
+# In[ ]:
+
+
+
+
