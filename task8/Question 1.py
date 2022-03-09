c073e4f2437387a238bdf83bfcd163f1e7422366
LakshmiSravya08 <99632838+LakshmiSravya08@users.noreply.github.com>
2022-03-09 20:12:16 +0530

    Add files via upload

---
+++ task8/Question 1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+from numpy import *
+a=array([])
+b=int(input('Enter the fFirst Number:'))
+c=int(input('Enter the last number Number:'))
+for p in range(b,c):
+    a=append(a,p)
+    for p in range(5):
+        a=append(a,0)
+a=append(a,c)
+print(a)
+
+
+# In[ ]:
+
+
+
+
